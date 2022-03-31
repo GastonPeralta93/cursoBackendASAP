82273edef225b60c073f77fbcd344bf509e0bae7
Gaston Peralta <gastonperalta93@gmail.com>
2022-03-31 17:37:24 -0300

    agregando JSTL

--- HolaSpringFrameWork/src/main/java/com/domain/controller/IndexController.java
+++ HolaSpringFrameWork/src/main/java/com/domain/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.domain.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +13,24 @@ public class IndexController {
 	@RequestMapping("/home")
 	public String goIndex() {
 		return "Index";
+	}
+	
+	@RequestMapping("/")
+	public String goPresentacion() {
+		return "Presentacion";
+	}
+	
+	@RequestMapping("/Listado")
+	public String goListado(Model model) {
+		List<String> alumnos = new ArrayList<String>();
+		alumnos.add("Juan");
+		alumnos.add("Pedro");
+		alumnos.add("José");
+		
+		model.addAttribute("titulo", "Listado de alumnos");
+		model.addAttribute("profesor", "Gabriel Casas");
+		model.addAttribute("alumnos", alumnos);
+		
+		return "Listado";
 	}
 }
