68f39b05b0feac4e9c48c16a6ece7bf44d8efdf5
Gaston Peralta <gastonperalta93@gmail.com>
2022-04-05 00:51:00 -0300

    Auditoria

---
+++ Practica0/src/ar/edu/asap/practica0/modelo/Auditoria.java
@@ -0,0 +1,39 @@
+package ar.edu.asap.practica0.modelo;
+
+import java.util.Date;
+
+public class Auditoria {
+
+	private Date fecha;
+	private int cantidadJugadas = 0;
+	
+	public Auditoria() {
+
+	}
+
+	public Auditoria(Date fecha, int cantidadJugadas) {
+		super();
+		this.fecha = fecha;
+		this.cantidadJugadas = cantidadJugadas;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public int getCantidadJugadas() {
+		return cantidadJugadas;
+	}
+
+	public void setCantidadJugadas(int cantidadJugadas) {
+		this.cantidadJugadas = cantidadJugadas;
+	}
+	
+	public void contarDespuesDeLaJugada() {
+		cantidadJugadas = cantidadJugadas + 1;
+	}
+}
---
+++ Practica0/src/ar/edu/asap/practica0/modelo/Jugador.java
@@ -0,0 +1,88 @@
+package ar.edu.asap.practica0.modelo;
+
+import java.util.Objects;
+
+public class Jugador {
+	
+	private int codigo;
+	private String nombre;
+	private String nickname;
+	private PiedraPapelTijeraFactory jugadaElegida;
+
+
+	public Jugador() {
+		super();
+	}
+	
+	public Jugador(int codigo, String nombre, String nickname, PiedraPapelTijeraFactory jugadaElegida) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.nickname = nickname;
+		this.jugadaElegida = jugadaElegida;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public PiedraPapelTijeraFactory getJugadaElegida() {
+		return jugadaElegida;
+	}
+
+	public void setJugadaElegida(PiedraPapelTijeraFactory jugadaElegida) {
+		this.jugadaElegida = jugadaElegida;
+	}
+	
+	//+ hashCode int
+	
+	//+ equals(Object): boolean
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo, jugadaElegida, nickname, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jugador other = (Jugador) obj;
+		return codigo == other.codigo && Objects.equals(jugadaElegida, other.jugadaElegida)
+				&& Objects.equals(nickname, other.nickname) && Objects.equals(nombre, other.nombre);
+	}
+
+	
+	
+	public String toString() {
+		return getCodigo() + "-" + getNombre() + "-" + getNickname() + "-" + getJugadaElegida();
+	}
+	
+	
+
+
+}
