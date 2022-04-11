50990b129cc793955c35fefb69281b1423b187ac
Gaston Peralta <gastonperalta93@gmail.com>
2022-04-11 19:27:41 -0300

    practica 6

---
+++ DemoConsola/src/main/java/com/gabrielCode/DemoConsolaApplication.java
@@ -0,0 +1,35 @@
+package com.gabrielCode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gabrielCode.service.IPersonaService;
+import com.gabrielCode.service.PersonaServiceImpl;
+
+@SpringBootApplication
+public class DemoConsolaApplication implements CommandLineRunner {
+
+	private static Logger log = LoggerFactory.getLogger(DemoConsolaApplication.class);
+	
+	@Autowired
+	private IPersonaService service;
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConsolaApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("impresion en la consola");
+		//log.info("impresion en consola");
+		//log.warn("esto es una advertencia");
+		//log.error("esto es un error");
+		//service = new PersonaServiceImpl();
+		service.registrarHandler("Gabriel");
+	}
+}
---
+++ DemoConsola/src/main/java/com/gabrielCode/repository/IPersona.java
@@ -0,0 +1,7 @@
+package com.gabrielCode.repository;
+
+public interface IPersona {
+	public void registrar(String pNombre);
+}
+
+
